0359fb9e31bb18645a0ea3c4007938daa88630c5
Dami3008 <tomasmunguia300821@hotmail.com>
2023-08-01 11:47:19 -0300

    cambio main.cpp

--- prueba/main.cpp
+++ prueba/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Hola, aguante Argentina" << endl;
-    cout << "Aguante ser campeón del Mundo" << endl;
+    cout << "Aguante ser campeon del Mundo" << endl;
+    system("cls");
     return 0;
 }
